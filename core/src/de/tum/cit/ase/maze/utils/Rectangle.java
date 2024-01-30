34d79b1b81ca19a514cd92666c6e733b26d6db89
HenrikDuerrfeld <henrik.duerrfeld.20002@gmail.com>
2024-01-30 12:00:22 +0100

    player orientation changes but movement still lacks

--- core/src/de/tum/cit/ase/maze/utils/Rectangle.java
+++ core/src/de/tum/cit/ase/maze/utils/Rectangle.java
@@ -10,4 +10,16 @@ public class Rectangle {
         this.w = w;
         this.h = h;
     }
+    //collide function to detect player and wall collisions
+    public boolean collide(Rectangle b) {
+        Rectangle rectangle = this;
+        Rectangle r = b;
+        if (r.x < rectangle.x + rectangle.w &&
+                r.x + r.w > rectangle.x &&
+                r.y < rectangle.y + rectangle.h &&
+                r.h + r.y > rectangle.y) {
+            return true;
+        }
+        return false;
+    }
 }
