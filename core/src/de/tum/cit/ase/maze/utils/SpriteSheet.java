38c8442c73523e4c54b732e5ceb2723d3d7fec0b
HenrikDuerrfeld <henrik.duerrfeld.20002@gmail.com>
2024-01-30 14:58:12 +0100

    sprite sheet is done

--- core/src/de/tum/cit/ase/maze/utils/SpriteSheet.java
+++ core/src/de/tum/cit/ase/maze/utils/SpriteSheet.java
@@ -6,30 +6,37 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
+//https://www.youtube.com/watch?v=ONe91ro51cQ
+//https://libgdx.com/wiki/graphics/2d/2d-animation
+// we are using spritesheet from libgdx to splice our jpeg files into different frames anf for animation purposes
 public class SpriteSheet {
-    Texture texture; // this the texture of our sprite sheet
-    int rows, cols; // number of rows and columns in our sprite sheet grid
-    TextureRegion[] frames; // we use this to choose and store the pictures from our selected file
-    int width,height;
+    // https://libgdx.com/wiki/graphics/2d/spritebatch-textureregions-and-sprites
+    Texture texture; // this decodes our image files so that we can use them in our game
+
+    int rows,cols;
+
+    TextureRegion[] frames;
+    //animation control variables
+    float countTime = 0;
     int from;
     int to;
     int current;
     float time;
     boolean loop;
-    int countTime = 0;
+    int width,height;
 
 
-    public SpriteSheet(Texture texture, int rows, int cols) {
-        this.texture = texture;
-        this.rows = rows;
+    public SpriteSheet(Texture texture,int rows,int cols){
+        this.texture = texture; // where the animations frames are contained
+        this.rows = rows; // number of rows and cols in the sprite sheet
         this.cols = cols;
 
-        frames = new TextureRegion[rows * cols];
+        frames = new TextureRegion[rows * cols]; // creating new array to store all the frames of the sprite sheet
         TextureRegion[][] tmp = TextureRegion.split(texture, texture.getWidth() / cols, texture.getHeight() / rows);  // split the sprite sheet
-        width = texture.getWidth() / cols;
+        width = texture.getWidth() / cols; // width and height calculates the values of a single frame
         height =  texture.getHeight() / rows;
         int ide = 0;
-        for(int i = 0; i < rows; i++) {
+        for(int i = 0; i < rows; i++) { // nested loop iterates over rows and columns to assign Tecture region to the frames array
             for (int j = 0; j < cols; j++) {
                 frames[ide] = tmp[i][j];
                 ide++;
@@ -38,15 +45,11 @@ public class SpriteSheet {
     }
     public TextureRegion getTexture(int frame) {
         return frames[frame];
-    } // we use this to get the frame we want from jpeg file
-    public float getWidth() {
-        return width;
     }
-    public float getHeight() {
-        return height;
-    }
-
-    // Will come to play when initializing character animation in class Player
+    public TextureRegion getCurrentFrame() {
+        return frames[current];
+    } //
+    //setPlay method is used to setup animations, eg. from frame x to frame y for time long and if loop true
     public void setPlay(int from, int to, float time, boolean loop) {
         this.from = from;
         this.to = to;
@@ -54,6 +57,8 @@ public class SpriteSheet {
         this.loop = loop;
         current = from;
     }
+    //play method updates the animation frame based on the elapsed time, if the elapsed time is grater then the time variable then the frame is incremented
+    //if loop is set to true then the animation is set to start again from the beginning
     public void play() {
         countTime += Gdx.graphics.getDeltaTime();
         if(countTime >= time) {
@@ -68,7 +73,17 @@ public class SpriteSheet {
         }
 
     }
-    public TextureRegion getCurrentFrame() {
-        return frames[current];
-    } //returns current frames used for player
+
+    public float getWidth() {
+        return width;
+    }
+    public float getHeight() {
+        return height;
+    }
+
+    void dispose()
+    {
+        texture.dispose();
+    }
+
 }
